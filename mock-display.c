3814149af9cb2e809c7608818ca00d0e5929c0e9
Torkild Retvedt <torkild@retvedt.no>
2013-03-23 17:13:48 +0100

    Ported font rendering

--- mock-display.c
+++ mock-display.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <curses.h>
+#include <string.h>
+#include <time.h>
 #include "font.h"
 
+#define X_MAX 128
+
 // 128 columns of 8 bit pixel rows
-unsigned char display_memory[128];
+unsigned char display_memory[X_MAX];
 
 void render_memory()
 {
@@ -12,7 +16,7 @@ void render_memory()
     for (row = 0; row < 8; row++)
     {
         int col;
-        for (col = 0; col < 128; col++)
+        for (col = 0; col < X_MAX; col++)
         {
             if (display_memory[col] & (1 << row))
                 addch('x');
@@ -35,29 +39,44 @@ void setup_curses()
     keypad(stdscr, TRUE);
 }
 
-void hardcoded_pixels()
+int render_char(char c, int x)
 {
-    display_memory[0] = 0xff;
-    display_memory[127] = 0x01;
+    char *substr = strchr(charLookup, c);
+    if (substr == NULL)
+        return 0;
 
-    // $
-    display_memory[23] = 0x24;
-    display_memory[24] = 0x2a;
-    display_memory[25] = 0x7f;
-    display_memory[26] = 0x2a;
-    display_memory[27] = 0x12;
+    int index = substr - charLookup;
+    int width = font_variable[index][0];
 
-    // a
-    display_memory[100] = 0x20;
-    display_memory[101] = 0x54;
-    display_memory[102] = 0x54;
-    display_memory[103] = 0x78;
+    int col;
+    for (col = 0; col <= width; col++) {
+        // dont write to the display buffer if the location is out of range
+        if((x + col) >= 0 && (x + col) < X_MAX){
+            // reads entire row of glyph, jams it into memory
+            if (col != width)
+                display_memory[x+col] = font_variable[index][col+1];
+            else
+                display_memory[x+col] = 0;
+        }
+    }
+
+    return width;
+}
+
+void render_string(char * text, int x)
+{
+    int length = strlen(text);
+    int i;
+    for (i=0; i < length; i++)
+    {
+        int width = render_char(text[i], x);
+        x += width + 1;
+    }
 }
 
 int main()
 {
     setup_curses();
-    hardcoded_pixels();
 
     int done = 0;
 
@@ -75,9 +94,17 @@ int main()
             }
         }
 
+        memset(display_memory, 0, sizeof(display_memory));
+
+        time_t current_time = time(NULL);
+        struct tm *local_time = localtime(&current_time);
+        char *time_string = asctime(local_time);
+
+        render_string(time_string, 0);
+
         clear();
         render_memory();
-        halfdelay(2);
+        halfdelay(1);
     }
 
     endwin();
