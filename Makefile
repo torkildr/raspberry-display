de9bbaf838340fc394a18ff2e0f11a22a55a28eb
Torkild Retvedt <torkild@retvedt.no>
2013-03-24 16:10:27 +0100

    Splittier code than ever. Should just be to link to the actual implentation instead.

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
 HEADERS=font.h display.h
-CFLAGS=-Wall
+CFLAGS=-Wall -g
 
-default: mock-display
+default: mock-curses-client
 
 font_render.o: display.c $(HEADERS)
-	cc -g $(CFLAGS) -o display.o -c display.c
+	cc $(CFLAGS) -o display.o -c display.c
 
-mock-display: mock-display.c display.o $(HEADERS)
-	cc -g $(CFLAGS) $< display.o -lncurses -o $@
+mock-display.o: mock-display.c $(HEADERS)
+	cc $(CFLAGS) -o mock-display.o -c mock-display.c
+
+mock-curses-client: curses-client.c display.o mock-display.o $(HEADERS)
+	cc $(CFLAGS) $< display.o mock-display.o -lncurses -o $@
 
 clean:
-	rm -f mock-display
+	rm -f mock-curses-client
+	rm -f curses-client
 	rm -f *.o
+
--- mock-display.c
+++ mock-display.c
@@ -1,101 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <curses.h>
-#include <string.h>
-#include <time.h>
 #include "display.h"
-
-void setup_curses()
-{
-    initscr();
-    timeout(0);
-    cbreak();
-    noecho();
-
-    nonl();
-    intrflush(stdscr, FALSE);
-    keypad(stdscr, TRUE);
-}
-
-char *time_string()
-{
-    time_t current_time = time(NULL);
-    struct tm *local_time = localtime(&current_time);
-    return asctime(local_time);
-}
-
-char *abc_string()
-{
-    return " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\xe6\xf8\xe5\xc6\xd8\xc5";
-}
-
-int main()
-{
-    setup_curses();
-
-    int done = 0;
-    int offset = 0;
-    char * (*get_text)();
-
-    get_text = &time_string;
-    timer_enable();
-
-    while (!done) {
-        int c;
-
-        if ((c = getch()) != ERR) {
-            switch (c) {
-                case 'q':
-                    done = 1;
-                    break;
-                case 't':
-                    get_text = &time_string;
-                    break;
-                case 'a':
-                    get_text = &abc_string;
-                    break;
-                case 'l':
-                    display_scroll(SCROLL_LEFT);
-                    break;
-                case 'r':
-                    display_scroll(SCROLL_RIGHT);
-                    break;
-                case 'd':
-                    display_scroll(SCROLL_DISABLED);
-                    break;
-                case '0':
-                case KEY_HOME:
-                    display_scroll(SCROLL_DISABLED);
-                    offset = 0;
-                    break;
-                case KEY_LEFT:
-                    offset -= 1;
-                    break;
-                case KEY_RIGHT:
-                    offset += 1;
-                    break;
-                case KEY_NPAGE:
-                    offset -= X_MAX;
-                    break;
-                case KEY_PPAGE:
-                    offset += X_MAX;
-                    break;
-            }
-        }
-
-        render_text(get_text(), offset);
-        halfdelay(2);
-    }
-
-    timer_disable();
-    endwin();
-    return 0;
-}
 
 //
 // mocked stuff from display.h
 //
-//
+
 void display_clear()
 {
     clear();
