626f842fc831f96de030a61d9d011eec28766677
Torkild Retvedt <torkild@retvedt.no>
2019-10-23 08:58:46 +0200

    update to wiringPi mirror

--- install-wiringPi.sh
+++ install-wiringPi.sh
@@ -2,7 +2,11 @@
 
 set -xe
 
-git clone git://git.drogon.net/wiringPi
+#git clone git://git.drogon.net/wiringPi
+
+# drogon.net seems out of commission, use unofficial mirror
+git clone https://github.com/torkildr/wiringPi
+
 cd wiringPi
 ./build
 
